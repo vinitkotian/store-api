958864d6538676118a505dc5b8064236233637eb
Vinit Kotian <kotianvinit@gmail.com>
2022-02-07 22:13:33 +0530

    Adding filter criteria to GET product api

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -2,13 +2,8 @@ const productModel = require("../models/product-model");
 const { createCustomApiError } = require("../errors/custom-api-error");
 
 const getProductList = async (req, res, next) => {
-  const { featured } = req.query;
-  queryParams = {};
-  if (featured) {
-    queryParams = req.query;
-  }
-  const productList = await productModel.find(queryParams);
-  if (productList) {
+  const productList = await productModel.find(req.query);
+  if (productList.length) {
     res.status(200).json({
       status: "Success",
       statusCode: 200,
@@ -16,7 +11,7 @@ const getProductList = async (req, res, next) => {
     });
   } else {
     let error = createCustomApiError(
-      "No products math the given criteria.",
+      "No products match the given criteria.",
       404,
       "Success"
     );
